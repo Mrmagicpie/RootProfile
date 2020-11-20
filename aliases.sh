3f422a96495566f8907ed1c4a58d0ec00eac599c
Mrmagicpie <realmrmagicpie@gmail.com>
2020-11-20 02:20:59 +0000

    Initials Commit V2

---
+++ aliases.sh
@@ -0,0 +1,26 @@
+
+# LS Commands
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -a -CF'
+#alias l='ls -CF'
+
+# Misc
+
+alias py='python3'
+
+# CD Alternatives
+
+alias adir='cd /etc/apache2/sites-available'
+alias vdir='cd /var/www'
+alias aqua='cd /var/www/AquaHQ'
+alias isthicc='cd /var/www/IsThicc.xyz'
+alias bois='cd /var/www/BoisBot'
+
+#############################################################
+
+
+alias doge='''echo """
+doge is omega cute
+"""'''
---
+++ includes.sh
@@ -0,0 +1,7 @@
+if [ -f /root/root-config/aliases.sh ]; then
+    . /root/root-config/aliases.sh
+fi
+
+if [ -f /root/root-config/bash.sh ]; then
+    . /root/root-config/bash.sh
+fi
