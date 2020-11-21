bd13d40219174f891b8097b99d632a3655fd16d2
Mrmagicpie <realmrmagicpie@gmail.com>
2020-11-21 03:42:06 +0000

    Setup/README

--- README.md
+++ README.md
@@ -1,14 +1,40 @@
-# RootProfile
+<h1 align="center">Mrmagicpie's RootProfile</h1>
 
-Hey! This is my root setting while on Linux(Ubuntu).
+Hey there, this is the settings I use for my root user. Feel free to take a look around. :D
 
 ## How to use:
+
+### Option 1:
+
+Use this handy script I provided [here](https://root.mrmagicpie.xyz/setup.sh), run this as **root**.
+
+### Option 2:
+
+Use the following commands to setup manually.
+
+```
+cd /root
+```
 
 ```
 git clone https://github.com/Mrmagicpie/RootProfile root-config
 ```
-then,
+
 ```
-rm .bashrc && mv root-config/bashrc ./.bashrc
+rm .bashrc
 ```
+
+```
+mv root-config/.bashrc ./.bashrc
+```
+
 Then you're done!
+
+
+## How to contribute:
+
+Review our CONTRIBUTING.md to help us! On our [website](https://root.mrmagicpie.xyz/CONTRIBUTING) or our [GitHub](https://github.com/Mrmagicpie/RootProfile/blob/main/CONTRIBUTING.md).
+
+<br>
+<br>
+<p align="center">Copyright (c) 2020 Mrmagicpie</p>
---
+++ setup.sh
@@ -0,0 +1,71 @@
+#
+#
+# RootProfile Setup.sh | Copyright (c) 2020 Mrmagicpie
+#
+#
+
+echo "|---------------------------------|"
+echo "|   Welcome to Mrmagicpie's root  |"
+echo "|   config setup. Would you like  |"
+echo "|   to proceed with this setup?   |"
+echo "|                                 |"
+echo "| WARNING:		        |"
+echo "| This will clear all your current|"
+echo "|    '/root/.bashrc' settings!    |"
+echo "| Please stash all changes before |"
+echo "|           you begin!	 	|"
+echo "| 				|"
+echo "| Please say 'y' for yes, and 'n' |"
+echo "|            for no!		|"
+echo "|---------------------------------|"
+
+read yes_no
+
+if [ "$yes_no" == "n" ] || [ "$yes_no" == "no" ]; then
+
+	echo "|----------------|"
+	echo "| Exiting setup! |"
+	echo "|----------------|"
+
+	exit
+
+elif [ "$yes_no" != "y" ]; then
+
+	echo "|-----------------|"
+	echo "| Invalid option! |"
+        echo "|-----------------|"
+
+	exit
+
+fi;
+
+
+echo "|------------------------|"
+echo "| Continuing with setup. |"
+echo "|------------------------|"
+
+sleep 1
+
+cd /root
+
+if [ -f /root/.bashrc ]; then
+    mv .bashrc default-bashrc.ubuntu
+fi;
+
+git clone https://github.com/Mrmagicpie/RootProfile root-config
+mv root-config/.bashrc ./.bashrc
+
+
+
+echo "|---------------------------------------------|"
+echo "| Finished the Mrmagicpie Root Profile setup! |"
+echo "|                                             |"
+echo "|        Report issues on our GitHub:         |"
+echo "|   https://github.com/Mrmagicpie/RootProfile |"
+echo "|---------------------------------------------|"
+
+sleep 3
+
+
+
+
